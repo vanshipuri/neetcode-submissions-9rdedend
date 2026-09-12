6bc3c9c073846aef71bb1705464a66a77379f9e1
Vanshi Puri <148044987+vanshipuri@users.noreply.github.com>
2026-09-12 13:12:43 +0530

    Add: is-palindrome - submission-20

---
+++ Data Structures & Algorithms/is-palindrome/submission-20.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        res=[]
+        for chr in s:
+            if chr.isalnum():
+                res.append(chr.lower())
+        return res==res[::-1]
+        
